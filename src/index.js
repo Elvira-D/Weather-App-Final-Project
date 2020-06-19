79be8adba692c75a86d4f37d7ff8113722083549
Elvira-D <66033824+Elvira-D@users.noreply.github.com>
2020-06-19 15:24:12 +0100

    Defined min & max temperatures variables in DisplayTemperature function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 function displayTemperature(response) {
-let temperatureElement = document.querySelector("#current-temperature");
-let cityElement = document.querySelector("#city");
+    let temperatureElement = document.querySelector("#current-temperature");
+    let cityElement = document.querySelector("#city");
+    let minTemp = document.querySelector("#min");
+    let maxTemp = document.querySelector("#max");
     temperatureElement.innerHTML = `Current Temperature ${Math.round(response.data.main.temp)}°`;
-cityElement.innerHTML = response.data.name; 
+    cityElement.innerHTML = response.data.name;
+    minTemp.innerHTML = Math.round(response.data.main.temp_min);
+    maxTemp.innerHTML = Math.round(response.data.main.temp_max);
+
 }
 
 let apiKey = "4820a880cf32920cd905490b92fa0630";
