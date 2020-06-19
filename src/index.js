808cbe1f76e94e941e4c07b38e1f611f8da2c4c1
Elvira-D <66033824+Elvira-D@users.noreply.github.com>
2020-06-19 17:55:57 +0100

    Added descriptionElement variable

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 function displayTemperature(response) {
+    console.log(response.data);
     let temperatureElement = document.querySelector("#current-temperature");
     let cityElement = document.querySelector("#city");
     let minTemp = document.querySelector("#min");
     let maxTemp = document.querySelector("#max");
+    let descriptionElement = document.querySelector("#description");
     temperatureElement.innerHTML = `Current Temperature ${Math.round(response.data.main.temp)}°`;
     cityElement.innerHTML = response.data.name;
     minTemp.innerHTML = Math.round(response.data.main.temp_min);
     maxTemp.innerHTML = Math.round(response.data.main.temp_max);
+    descriptionElement.innerHTML = response.data.weather[0].description;
 
 }
 
