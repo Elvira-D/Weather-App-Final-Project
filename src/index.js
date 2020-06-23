bb2147cf59ee58b948c0e9f86c5bb6daf2932517
Elvira-D <66033824+Elvira-D@users.noreply.github.com>
2020-06-23 15:19:07 +0100

    Added function Search

--- src/index.js
+++ src/index.js
@@ -41,16 +41,20 @@ function displayTemperature(response) {
 
 }
 
+function search(city) {
+    let apiKey = "4820a880cf32920cd905490b92fa0630";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    
+    axios.get(apiUrl).then(displayTemperature)
+
+}
+
 function handleSubmit(event) {
     event.preventDefault();
     let cityInput = document.querySelector("#search-city");
     console.log(cityInput.value);
 }
 
-let apiKey = "4820a880cf32920cd905490b92fa0630";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
-
-axios.get(apiUrl).then(displayTemperature)
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
